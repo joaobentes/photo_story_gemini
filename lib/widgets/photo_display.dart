2daf4c50c0b74a31207d44f1440dd23f7c59221f
Joao Bentes <joaobentes.junior@gmail.com>
2024-06-09 19:07:15 +0200

    refactor: New widgets

---
+++ lib/widgets/photo_display.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:photo_story_gemini/providers/photo_author_provider.dart';
+
+class PhotoDisplay extends ConsumerWidget {
+  final bool readOnly;
+
+  const PhotoDisplay({super.key, this.readOnly = false});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Center(
+      child: Stack(
+        alignment: Alignment.topRight,
+        children: [
+          SizedBox(
+            height: 400,
+            child: Image.file(
+              ref.read(photoAuthorProvider).photo!,
+              fit: BoxFit.cover,
+            ),
+          ),
+          if (!readOnly)
+            IconButton(
+                icon: const Icon(Icons.delete, color: Colors.redAccent),
+                onPressed: () {
+                  ref.read(photoAuthorProvider.notifier).setPhoto(null);
+                }),
+        ],
+      ),
+    );
+  }
+}
